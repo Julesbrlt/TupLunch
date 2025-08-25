609081abe3f713522c5c5876833ef29a4afdd3f4
Charlotte Constance <charlotteconstance7@gmail.com>
2025-08-25 12:20:10 +0200

    recipe model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_25_094753) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_25_095431) do
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "price_per_unit"
+    t.integer "unit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "time"
+    t.integer "calories"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,25 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_25_094753) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-end
 
-  ActiveRecord::Schema[7.1].define(version: 2025_08_25_094942) do
-ActiveRecord::Schema[7.1].define(version: 2025_08_25_095431) do
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "price"
-    t.integer "time"
-    t.integer "calories"
-  end
-end
-
-ActiveRecord::Schema[7.1].define(version: 2025_08_25_094942) do
-  create_table "ingredients", force: :cascade do |t|
-    t.string "name"
-    t.integer "price_per_unit"
-    t.integer "unit"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
