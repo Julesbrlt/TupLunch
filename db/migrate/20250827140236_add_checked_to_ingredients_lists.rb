bb5100d9efa58b21bb2c526cbed64e5a4f814279
PierreLouis Ruillier <pierrelouis.ruillier@kedgebs.com>
2025-08-27 17:16:43 +0200

    ingredient list + scss

--- db/migrate/20250827140236_add_checked_to_ingredients_lists.rb
+++ db/migrate/20250827140236_add_checked_to_ingredients_lists.rb
@@ -1,5 +1,5 @@
 class AddCheckedToIngredientsLists < ActiveRecord::Migration[7.1]
   def change
-    add_column :ingredients_lists, :checked, :boolean
+    add_column :ingredients_lists, :checked, :boolean, null: false, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_27_101147) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_27_140236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_27_101147) do
     t.bigint "recap_recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "checked"
     t.index ["recap_recipe_id"], name: "index_ingredients_lists_on_recap_recipe_id", unique: true
   end
 
