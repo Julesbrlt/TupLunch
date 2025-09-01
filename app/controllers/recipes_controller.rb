a27776b51210c8418703b4b4cd7383ef7328e3c5
Charlotte Constance <charlotteconstance7@gmail.com>
2025-09-01 17:49:49 +0200

    compteur

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
+    @profile = current_user.profile
+    @choice_recipes = @profile.total_recipes
   end
 
   def show
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,8 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import FavoritesController from "./favorites_controller"
+import CounterController from "./counter_controller"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 application.register("favorites", FavoritesController)
+application.register("counter", CounterController)
