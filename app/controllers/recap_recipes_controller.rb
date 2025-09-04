9a82118d2372df59bf33e2dd359a3b1df552c6d0
Jules Baralotto <jules.baralotto@kedgebs.com>
2025-09-04 15:36:14 +0200

    some css modifs on dashboard and page swipe

--- app/controllers/recap_recipes_controller.rb
+++ app/controllers/recap_recipes_controller.rb
@@ -9,17 +9,20 @@ class RecapRecipesController < ApplicationController
     @profile = current_user.profile
     @recipe = Recipe.find(params[:recipe_id])
     RecapRecipe.find_or_create_by!(recipe: @recipe, user: current_user)
-    # @recap_recipes = current_user.recap_recipes.includes(:recipe)
-    # if @profile.total_recipes >= @recap_recipes.count
-    #   redirect_to recap_recipes_path
-    # end
 
-    # total_price = @recap_recipes.sum { |rr| rr.recipe.price }
-    # total_calories = @recap_recipes.sum { |rr| rr.recipe.calories }
+    @recap_recipes = current_user.recap_recipes.includes(:recipe)
+    puts " Profile Total Recipes : #{@profile.total_recipes}"
+        puts " Recap Recipes count : #{@recap_recipes.count}"
+    if @recap_recipes.count >= @profile.total_recipes
+      render json: { redirect_url: recap_recipes_path } and return
+    end
 
-    # if total_price > @profile.total_budget || total_calories > @profile.total_calories
-    # flash.now[:alert] = "⚠️ Attention vous dépassez votre budget ou vos calories"
-    # end
+    total_price = @recap_recipes.sum { |rr| rr.recipe.price }
+    total_calories = @recap_recipes.sum { |rr| rr.recipe.calories }
+
+    if total_price > @profile.total_budget || total_calories > @profile.total_calories
+    flash.now[:alert] = "⚠️ Attention vous dépassez votre budget ou vos calories"
+    end
   end
 
   def destroy
